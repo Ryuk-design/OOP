bb3815d4d02f64c401a1b2c258f7998fccc8273e
Samuel Ahumada <samuel.ahumada-f@mail.escuelaing.edu.co>
2026-01-05 10:40:17 -0500

    One piece project

---
+++ One piece characters/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        Mugiwara luffy = new Mugiwara();
+            luffy.nombre = "Luffy";
+            luffy.edad = 19;
+            luffy.tripulacion();
+    
+    }
+
+}
---
+++ One piece characters/Mugiwara.java
@@ -0,0 +1,7 @@
+public class Mugiwara {
+    String nombre;
+    int edad;
+    static void tripulacion(){
+        System.out.println("¡Yo pertenezco a los mugiwara no ichimi!");
+    }  
+}
